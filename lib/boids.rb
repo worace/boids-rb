583d8a9634f7d97f975191e757c48520ed423260
worace <horace.d.williams@gmail.com>
2015-04-22 19:29:18 -0600

    wrap birds if they go out of bounds

--- lib/boids.rb
+++ lib/boids.rb
@@ -1,9 +1,10 @@
 require "securerandom"
 load_library :vecmath
 def setup
+  @bounds = Vec2D.new(800, 800)
   # generate a few birds
-  size 800, 800
-  @flock = Flock.new(10, rand(800), rand(800))
+  size @bounds.x, @bounds.y
+  @flock = Flock.new(10, rand(800), rand(800), @bounds)
   fill 255
 end
 
@@ -18,8 +19,8 @@ end
 
 class Flock
   attr_reader :birds
-  def initialize(bird_count, x, y)
-    @birds = Array.new(bird_count) { Bird.new(x + rand(-50..50), y + rand(-50..50), nil) }
+  def initialize(bird_count, x, y, bounds)
+    @birds = Array.new(bird_count) { Bird.new(Vec2D.new(x + rand(-50..50), y + rand(-50..50)), bounds) }
   end
 end
 
@@ -34,10 +35,13 @@ end
 # render itself -- generate a triangle pointing in the right direction
 # and at the right position -- maybe to start each bird is represented by a circle?
 class Bird
-  attr_reader :position, :velocity
+  attr_reader :position, :velocity, :bounds
 
-  def initialize(x,y,heading)
-    @position = Vec2D.new(x,y)
+  def initialize(position, bounds)
+    @acceleration = Vec2D.new(rand(100), rand(100))
+    @acceleration.set_mag(1)
+    @position = position
+    @bounds = bounds
     @velocity = Vec2D.new
   end
 
@@ -70,14 +74,29 @@ class Bird
     Vec2D.new(mouse_x, mouse_y)
   end
 
+  def wrap_if_out_of_bounds
+    if position.x > bounds.x
+      position.x = 0
+    elsif position.x < 0
+      position.x = bounds.x
+    end
+
+    if position.y > bounds.y
+      position.y = 0
+    elsif position.y < 0
+      position.y = bounds.y
+    end
+  end
+
   def draw
-    accel = mouse_heading - position
-    accel.set_mag(0.5)
-    @velocity += accel
+    #accel = mouse_heading - position
+    #accel.set_mag(0.5)
+    @velocity += @acceleration
     if velocity.mag > max_speed
       velocity.set_mag(max_speed)
     end
     @position = position + velocity
+    wrap_if_out_of_bounds
     #puts "position is vec2d with x #{position.x}, y #{position.y}, heading #{position.heading}, magnitude #{position.mag}"
     ellipse(@position.x, @position.y, 10, 10)
   end
